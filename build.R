80d7827c698404f2581da893af23710df09eb412
jemus42 <jemus42@quantenbrot.de>
2017-02-03 10:57:23 +0100

    Fix build script, add assets ¯\_(ツ)_/¯

--- build.R
+++ build.R
@@ -2,7 +2,12 @@
 
 rmarkdown::render(input         = "presentation.Rmd",
                   output_format = "ioslides_presentation",
+                  output_file   = "presentation_ioslides.html")
+
+rmarkdown::render(input         = "presentation.Rmd",
+                  output_format = "xaringan::moon_reader",
                   output_file   = "presentation.html")
+
 
 # html_document themes
 rmarkdown::render(input         = "presentation.Rmd",
@@ -12,7 +17,7 @@ rmarkdown::render(input         = "presentation.Rmd",
 current_user <- system(command = "whoami", intern = T)
 
 if (current_user == "Lukas") {
-  out_dir <- "~/Sync/public.tadaa-data.de/QM/pruefungstutorien/qm1_wise_16/"
+  out_dir <- "~/Sync/public.tadaa-data.de/QM/pruefungstutorien/qm1_wise_17/"
 } else if (current_user == "tobias") {
   out_dir <- ""
 } else {
@@ -24,7 +29,7 @@ if (out_dir == "") {
 }
 
 out_docs   <- c(list.files(pattern = "*.html"), "presentation.Rmd")
-out_assets <- c("styles.css", "assets", "img")
+out_assets <- c("styles.css", "assets")
 
 sapply(out_docs,   file.copy, to = out_dir, overwrite = T, recursive = F)
 sapply(out_assets, file.copy, to = out_dir, overwrite = T, recursive = T)
