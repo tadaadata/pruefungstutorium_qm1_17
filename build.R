2b0e1278aa43ba5ba1da7cd03f6679916181e78b
jemus42 <jemus42@quantenbrot.de>
2017-02-04 13:25:04 +0100

    Build script tweaks

--- build.R
+++ build.R
@@ -9,7 +9,7 @@ rmarkdown::render(input         = "presentation.Rmd",
                   output_format = "html_document",
                   output_file   = "index.html")
 
-current_user <- system(command = "whoami", intern = T)
+current_user <- as.list(Sys.info())$user
 
 if (current_user == "Lukas") {
   out_dir <- "~/Sync/public.tadaa-data.de/QM/pruefungstutorien/qm1_wise_17/"
@@ -24,7 +24,7 @@ if (out_dir == "") {
 }
 
 out_docs   <- c(list.files(pattern = "*.html"), "presentation.Rmd")
-out_assets <- c("styles.css", "assets")
+out_assets <- c("assets")
 
 sapply(out_docs,   file.copy, to = out_dir, overwrite = T, recursive = F)
 sapply(out_assets, file.copy, to = out_dir, overwrite = T, recursive = T)
